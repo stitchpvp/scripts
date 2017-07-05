a5b12808a244d797beb5893ac8ace98c4ce1bf6f
transfixiated <transfixiate@hotmail.com>
2017-07-05 16:06:29 -0700

    further changes

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -10,11 +10,11 @@ function hailed(NPC, Spawn)
 conversation = CreateConversation()
 	local choice = math.random(1, 2)
 	if choice == 1 then
-    Say(conversation, NPC, Spawn, "Ha, " .. GetName(spawn) .. " these guard's can't protect you for too long!".)
-    AddConversationOption(conversation, "Am I trapped?", )
+    Say(conversation, NPC, Spawn, "Ha, " .. GetName(spawn) .. " these guard's can't protect you for too long!")
+    AddConversationOption(conversation, "Am I trapped?",)
     StartConversation(conversation, NPC, Spawn, "Fight with dignity!")
 	elseif choice == 2 then
-	  Say(conversation, NPC, Spawn, " Did I just see a title?".)
+	  Say(conversation, NPC, Spawn, " Did I just see a title?")
 	
 	end
 end
