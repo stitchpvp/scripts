99656701a0b293f5b158a85fe21b43334d9ef2ba
Jonmichael Chambers <jon@tyrbo.net>
2017-07-05 19:47:55 -0600

    Variables are case sensitive

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -11,7 +11,7 @@ function hailed(NPC, Spawn)
 	local choice = math.random(1, 2)
 
 	if choice == 1 then
-    Say(conversation, NPC, Spawn, "Ha, " .. GetName(spawn) .. " these guard's can't protect you for too long!")
+    Say(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
     AddConversationOption(conversation, "Am I trapped?")
     StartConversation(conversation, NPC, Spawn, "Fight with dignity!")
 	elseif choice == 2 then
