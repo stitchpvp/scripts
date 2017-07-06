db5c2571e2f7c80fb5fad952a14f3e8712c12a7e
Jonmichael Chambers <jon@tyrbo.net>
2017-07-06 01:35:58 -0600

    Clean up

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -3,27 +3,27 @@ function spawn(NPC)
 end
 
 function respawn(NPC)
-	spawn(NPC)
+  spawn(NPC)
 end
 
 function hailed(NPC, Spawn)
   FaceTarget(NPC, Spawn)
 
-  conversation = CreateConversation()
-	local choice = math.random(1, 2)
+  local conversation = CreateConversation()
+  local choice = math.random(1, 2)
 
-	if choice == 1 then
-          AddConversationOption(conversation, "Am I trapped?", "trapped")
-          StartConversation(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
-	elseif choice == 2 then
-	  StartConversation(conversation, NPC, Spawn, " Did I just see a title?")
-    	  AddConversationOption(conversation, "You've got to be kidding.." )
+  if choice == 1 then
+    AddConversationOption(conversation, "Am I trapped?", "trapped")
+    StartConversation(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
+  elseif choice == 2 then
+    StartConversation(conversation, NPC, Spawn, "Did I just see a title?")
+    AddConversationOption(conversation, "You've got to be kidding..")
   end
 end
   
-function trapped (NPC, Spawn)
-  FaceTarget (NPC, Spawn)
+function trapped(NPC, Spawn)
+  FaceTarget(NPC, Spawn)
   
   Say(NPC, "Fight with some dignity!", Spawn)
-  PlayAnimation (NPC, 12214, Spawn, 1)  
+  PlayAnimation(NPC, 12214, Spawn, 1)
 end
