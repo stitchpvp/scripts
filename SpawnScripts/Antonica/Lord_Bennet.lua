b85cb30ed9f6615b23b4eab70718a25c5aa8884d
Jonmichael Chambers <jon@tyrbo.net>
2017-07-05 19:45:30 -0600

    Fixes Lord Bennet

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -7,14 +7,14 @@ function respawn(NPC)
 end
 
 function hailed(NPC, Spawn)
-conversation = CreateConversation()
+	conversation = CreateConversation()
 	local choice = math.random(1, 2)
+
 	if choice == 1 then
     Say(conversation, NPC, Spawn, "Ha, " .. GetName(spawn) .. " these guard's can't protect you for too long!")
-    AddConversationOption(conversation, "Am I trapped?",)
+    AddConversationOption(conversation, "Am I trapped?")
     StartConversation(conversation, NPC, Spawn, "Fight with dignity!")
 	elseif choice == 2 then
 	  Say(conversation, NPC, Spawn, " Did I just see a title?")
-	
 	end
 end
