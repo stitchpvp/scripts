d104d40c995afb78b85fb09fc73b9438c37360a8
transfixiated <transfixiate@hotmail.com>
2017-07-05 19:17:01 -0700

    Modified syntax correctly?

---
+++ SpawnScripts/Antonica/adistressedmerchant.lua
@@ -0,0 +1,17 @@
+function spawn(NPC)
+   
+end
+
+function hailed(NPC, Spawn)
+conversation = CreateConversation()
+   FaceTarget(NPC, Spawn)
+   StartConversation(conversation, NPC, Spawn, "I beg of you to help me. These foul beasts have blocked off my escape. Help me  " .. Getname(spawn) .. " and I'll help you in return. ")
+   AddConversationOption(conversation, "Yeah about that..", "scared" )
+	PlayAnimation (npc, 10844, player, 1)
+end
+
+function scared(NPC, Spawn)
+  FaceTarget(NPC, Spawn)
+  StartConversation(conversation, NPC, Spawn, "Oh no! Please I can't survive this much longer!")
+  PlayAnimation (npc, 11275, player, 1)
+  end
