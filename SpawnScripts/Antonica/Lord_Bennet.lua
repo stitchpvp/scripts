760e74e4a4d1c5e54787804d005b1684e105dfee
Jonmichael Chambers <jon@tyrbo.net>
2017-07-05 19:49:05 -0600

    Convert tabs to spaces

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -3,18 +3,18 @@ function spawn(NPC)
 end
 
 function respawn(NPC)
-	spawn(NPC)
+  spawn(NPC)
 end
 
 function hailed(NPC, Spawn)
-	conversation = CreateConversation()
-	local choice = math.random(1, 2)
+  conversation = CreateConversation()
+  local choice = math.random(1, 2)
 
-	if choice == 1 then
+  if choice == 1 then
     Say(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
     AddConversationOption(conversation, "Am I trapped?")
     StartConversation(conversation, NPC, Spawn, "Fight with dignity!")
-	elseif choice == 2 then
-	  Say(conversation, NPC, Spawn, " Did I just see a title?")
-	end
+  elseif choice == 2 then
+    Say(conversation, NPC, Spawn, " Did I just see a title?")
+  end
 end
