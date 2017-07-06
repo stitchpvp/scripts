adf2c221ff4eaa28ffd85257228207f22ccebbec
Jonmichael Chambers <jon@tyrbo.net>
2017-07-06 01:05:05 -0600

    Adds missing end for Lord Bennet

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -8,14 +8,17 @@ end
 
 function hailed(NPC, Spawn)
   FaceTarget(NPC, Spawn)
+
   conversation = CreateConversation()
 	local choice = math.random(1, 2)
+
 	if choice == 1 then
     AddConversationOption(conversation, "Am I trapped?", "trapped")
     StartConversation(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
 	elseif choice == 2 then
 	  StartConversation(conversation, NPC, Spawn, " Did I just see a title?")
     AddConversationOption(conversation, "You've got to be kidding.." )
+  end
 end
   
 function trapped (NPC, Spawn)
