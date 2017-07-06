6f1af81600e956e8e6df64b22cf35ab0c717965d
Transfixiated <Transfixiate@hotmail.com>
2017-07-06 00:16:08 -0700

    Update Lord_Bennet.lua (#7)
    * Update Lord_Bennet.lua
    
    Changed npc to NPC
    
    * Update Lord_Bennet.lua
    
    Changed player to Spawn

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -13,17 +13,17 @@ function hailed(NPC, Spawn)
 	local choice = math.random(1, 2)
 
 	if choice == 1 then
-    AddConversationOption(conversation, "Am I trapped?", "trapped")
-    StartConversation(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
+          AddConversationOption(conversation, "Am I trapped?", "trapped")
+          StartConversation(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
 	elseif choice == 2 then
 	  StartConversation(conversation, NPC, Spawn, " Did I just see a title?")
-    AddConversationOption(conversation, "You've got to be kidding.." )
+    	  AddConversationOption(conversation, "You've got to be kidding.." )
   end
 end
   
 function trapped (NPC, Spawn)
   FaceTarget (NPC, Spawn)
   
-  Say(npc, "Fight with some dignity!", Spawn)
-  PlayAnimation (npc, 12214, player, 1)  
+  Say(NPC, "Fight with some dignity!", Spawn)
+  PlayAnimation (NPC, 12214, Spawn, 1)  
 end
