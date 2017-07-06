10594d87dff04ae57279b34eb8be50dcf80cf8b9
transfixiated <transfixiate@hotmail.com>
2017-07-05 23:21:54 -0700

    How many changes can I make?

--- SpawnScripts/Antonica/Lord_Bennet.lua
+++ SpawnScripts/Antonica/Lord_Bennet.lua
@@ -1,3 +1,27 @@
-function hailed(NPC, Spawn) 
-  FaceTarget(NPC, Spawn) 
-end 
+
+function spawn(NPC)
+end
+
+function respawn(NPC)
+	spawn(NPC)
+end
+
+function hailed(NPC, Spawn)
+ FaceTarget(NPC, Spawn)
+ conversation = CreateConversation()
+	local choice = math.random(1, 2)
+	if choice == 1 then
+    AddConversationOption(conversation, "Am I trapped?", "trapped")
+    StartConversation(conversation, NPC, Spawn, "Ha, " .. GetName(Spawn) .. " these guard's can't protect you for too long!")
+	elseif choice == 2 then
+	  StartConversation(conversation, NPC, Spawn, " Did I just see a title?")
+    AddConversationOption(conversation, "You've got to be kidding.." )
+	end
+  
+function trapped (NPC, Spawn)
+    FaceTarget (NPC, Spawn)
+    
+    Say(npc, "Fight with some dignity!", Spawn)
+    PlayAnimation (npc, 12214, player, 1)
+  
+end
