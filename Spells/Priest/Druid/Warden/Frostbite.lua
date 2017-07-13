74b8e4e722d3f1720c68a7548f8adca48f6849a7
Jonmichael Chambers <jon@tyrbo.net>
2017-07-13 01:26:28 -0600

    Add Frostbite

---
+++ Spells/Priest/Druid/Warden/Frostbite.lua
@@ -0,0 +1,12 @@
+function cast(Caster, Target, MinDmg, MaxDmg, Debuff)
+  AddSpellBonus(Target, 201, Debuff)
+  SpellDamage(Target, 4, MinDmg, MaxDmg)
+end
+
+function tick(Caster, Target, MinDmg, MaxDmg)
+  SpellDamage(Target, 4, MinDmg, MaxDmg)
+end
+
+function remove(Caster, Target)
+  RemoveSpellBonus(Target)
+end
