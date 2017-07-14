0c06427c039789b04a19c3b5090f7cffa211afd3
Transfixiated <Transfixiate@hotmail.com>
2017-07-14 15:44:35 -0700

    Adjusted pathing delay math.random

--- SpawnScripts/Commonlands/Arconius.lua
+++ SpawnScripts/Commonlands/Arconius.lua
@@ -2,5 +2,5 @@ function hailed(NPC, Spawn)
 	FaceTarget(NPC, Spawn)
 	local conversation = CreateConversation()
   Say(NPC, "Oh Dear.. It seem's I have dropped something down the well.", Spawn)
-  SpawnSet(Spawn, "visual_state", 13167)
+  PlayAnimation(Spawn, 13167, Spawn, 1)
 end
