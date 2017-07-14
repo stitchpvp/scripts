3b830d8151cf2a67a517ae5e985f4c6933670efd
Transfixiated <Transfixiate@hotmail.com>
2017-07-14 14:03:38 -0700

    High Priest ValMarr

--- SpawnScripts/Commonlands/High_Priest_of_ValMarr.lua
+++ SpawnScripts/Commonlands/High_Priest_of_ValMarr.lua
@@ -1,3 +1,31 @@
+require('SpawnScripts/Generic/GenericSkeletonVoiceOvers');
+
+
+local spoke = false
+function spawn(NPC)
+spoke = false
+end
+
 function hailed(NPC, Spawn) 
   FaceTarget(NPC, Spawn) 
 end 
+
+function aggro(NPC, Spawn)
+  generic_aggro(NPC, Spawn)
+end
+
+function healthchanged(NPC, Spawn)
+  generic_healthchanged(NPC, Spawn)
+end
+
+function killed(NPC, Spawn)
+  generic_killed(NPC, Spawn)
+end
+
+function death(NPC, Spawn)
+  generic_death(NPC, Spawn)
+end
+
+function ResetSpoke(NPC)
+	spoke = false
+end
