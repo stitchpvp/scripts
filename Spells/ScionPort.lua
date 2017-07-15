ec6ec7bd3f41eb766677b9c2dd0fac0c166740ce
Jonmichael Chambers <jon@tyrbo.net>
2017-07-15 17:56:31 -0600

    Allows Scion Port to go both directions

--- Spells/ScionPort.lua
+++ Spells/ScionPort.lua
@@ -1,6 +1,10 @@
 function cast(caster, target)
   if IsPlayer(target) and HasItem(target, 6575) then
     RemoveItem(target, 6575)
-    Zone(GetZone(12), target, -2065.79, 13.12, -629.25)
-  end
+
+    if GetZoneName(GetZone(target)) == "commonlands" then
+      Zone(GetZone("antonica"), target, -2065.79, 13.12, -629.25)
+    else if GetZoneName(GetZone(target) == "antonica" then
+      Zone(GetZone("commonlands"), target, -12.64, -46.80, -417.97)
+    end
 end
