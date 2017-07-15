e43f1b2c60e90a38e9df5e5a3fe563990579ff6c
Jonmichael Chambers <jon@tyrbo.net>
2017-07-15 17:26:31 -0600

    More testing

--- ItemScripts/PVP/ShiftingBuckler.lua
+++ ItemScripts/PVP/ShiftingBuckler.lua
@@ -1,3 +1,12 @@
 function equipped(item, player)
-  Say(player, "So shifty")
+  AddProc(player, 2, 10, item)
+end
+
+function unequipped(item, player)
+  Say(player, "removing proc")
+  RemoveProc(player, item)
+end
+
+function proc(item, caster, attacker, proctype)
+  Say(caster, "Being shifty around " .. GetName(attacker))
 end
