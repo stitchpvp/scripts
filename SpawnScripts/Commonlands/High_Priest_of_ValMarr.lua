1b8c6691dc79a16575f7e1a5db9328c29125e561
Transfixiated <Transfixiate@hotmail.com>
2017-07-15 02:51:33 -0700

    lumberjacks

--- SpawnScripts/Commonlands/High_Priest_of_ValMarr.lua
+++ SpawnScripts/Commonlands/High_Priest_of_ValMarr.lua
@@ -1,9 +1,6 @@
 require('SpawnScripts/Generic/GenericSkeletonVoiceOvers');
 
-
-local spoke = false
 function spawn(NPC)
-spoke = false
 end
 
 function hailed(NPC, Spawn) 
@@ -17,9 +14,6 @@ end
 function healthchanged(NPC, Spawn)
   generic_healthchanged(NPC, Spawn)
 end
-function RandomCallOut(NPC, Spawn)
- generic_RandomCallOut(NPC, Spawn)
-end 
 
 function killed(NPC, Spawn)
   generic_killed(NPC, Spawn)
@@ -27,8 +21,4 @@ end
 
 function death(NPC, Spawn)
   generic_death(NPC, Spawn)
-end
-
-function ResetSpoke(NPC)
-	spoke = false
 end
