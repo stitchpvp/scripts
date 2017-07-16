85cbcbc4c8d5f73929573a8419c0f320369dede4
Jonmichael Chambers <jon@tyrbo.net>
2017-07-15 19:37:51 -0600

    Commonlands is capitalized in the db

--- Spells/ScionPort.lua
+++ Spells/ScionPort.lua
@@ -2,7 +2,7 @@ function cast(caster, target)
   if IsPlayer(target) and HasItem(target, 6575) then
     RemoveItem(target, 6575)
 
-    if GetZoneName(GetZone(target)) == "commonlands" then
+    if GetZoneName(GetZone(target)) == "Commonlands" then
       Zone(GetZone("antonica"), target, -2065.79, 13.12, -629.25)
     elseif GetZoneName(GetZone(target)) == "antonica" then
       Zone(GetZone("commonlands"), target, -12.64, -46.80, -417.97)
