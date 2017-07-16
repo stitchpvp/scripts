d78b2a0297720e0d4371f5dc56f890df02d7db9f
Jonmichael Chambers <jon@tyrbo.net>
2017-07-15 18:34:34 -0600

    Fixes broken spell

--- Spells/ScionPort.lua
+++ Spells/ScionPort.lua
@@ -4,7 +4,8 @@ function cast(caster, target)
 
     if GetZoneName(GetZone(target)) == "commonlands" then
       Zone(GetZone("antonica"), target, -2065.79, 13.12, -629.25)
-    else if GetZoneName(GetZone(target) == "antonica" then
+    else if GetZoneName(GetZone(target)) == "antonica" then
       Zone(GetZone("commonlands"), target, -12.64, -46.80, -417.97)
     end
+  end
 end
