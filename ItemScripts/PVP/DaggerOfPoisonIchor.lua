6479e575ac75ef0e63d340964071543dbf26adbd
Jonmichael Chambers <jon@tyrbo.net>
2017-07-15 22:34:01 -0600

    Update Dagger of Poison Ichor

--- ItemScripts/PVP/DaggerOfPoisonIchor.lua
+++ ItemScripts/PVP/DaggerOfPoisonIchor.lua
@@ -1,3 +1,12 @@
 function equipped(item, player)
-  Say(player, "With this weapon, at 1.8 times per minute, this proc has a " .. GetProcPercentageForWeapon(item, 1.8) .. " chance of triggering.")
+  local proc_chance = GetProcPercentageForWeapon(item, 1.8)
+  AddProc(player, 3, proc_chance, item)
+end
+
+function proc(item, caster, target, proctype)
+  ProcDamage(caster, target, "Poison Strike", 9, 200)
+end
+
+function unequipped(item, player)
+  RemoveProc(player, item)
 end
