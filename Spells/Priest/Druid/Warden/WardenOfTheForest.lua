0fc0f467cfe21cde3ecaa60be48e5307718f0575
Jonmichael Chambers <jon@tyrbo.net>
2017-07-16 02:49:39 -0600

    Add Warden of the Forest

---
+++ Spells/Priest/Druid/Warden/WardenOfTheForest.lua
@@ -0,0 +1,19 @@
+function cast(Caster, Target, Focus, Mit, Resist)
+  SetIllusion(Caster, 216)
+
+  AddSkillBonus(Caster, 2638198038, Focus)
+
+  AddSpellBonus(Caster, 204, Mit)
+
+  AddSpellBonus(Caster, 201, Resist)
+  AddSpellBonus(Caster, 202, Resist)
+  AddSpellBonus(Caster, 203, Resist)
+
+  AddSpellBonus(Caster, 628, 1)
+end
+
+function remove(Caster, Target)
+  ResetIllusion(Caster)
+  RemoveSpellBonus(Caster)
+  RemoveSkillBonus(Caster)
+end
