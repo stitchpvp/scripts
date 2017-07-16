193c2d43778f3875624ec3e560f66bf49d982f7c
Jonmichael Chambers <jon@tyrbo.net>
2017-07-15 21:30:56 -0600

    Bring the KB down a bit

--- Spells/Items/IdolOfSeismicDisruption.lua
+++ Spells/Items/IdolOfSeismicDisruption.lua
@@ -1,6 +1,6 @@
 function cast(caster, target)
   AddControlEffect(target, 5)
-  Knockback(caster, target, 4000, -30, 25, 0)
+  Knockback(caster, target, 4000, -20, 10, 0)
 end
 
 function remove(caster, target)
