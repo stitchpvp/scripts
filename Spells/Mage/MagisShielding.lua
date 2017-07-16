b23c91af046b56cfb2191f7e0849705ba09988af
Transfixiated <Transfixiate@hotmail.com>
2017-07-16 14:00:29 -0700

    Unholy Covenant

---
+++ Spells/Mage/MagisShielding.lua
@@ -0,0 +1,11 @@
+function cast(Caster, Target, StaBuff, WisBuff, ResistBuff)
+  AddSpellBonus(Caster, 1, StaBuff)
+  AddSpellBonus(Caster, 200, ResistBuff)
+  AddSpellBonus(Caster, 201, ResistBuff)
+  AddSpellBonus(Caster, 202, ResistBuff)
+  AddSpellBonus(Caster, 203, ResistBuff)
+end
+
+function remove(Caster, Target)
+  RemoveSpellBonus(Caster)
+end
