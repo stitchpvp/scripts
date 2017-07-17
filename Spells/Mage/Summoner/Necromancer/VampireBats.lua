9e0f36acdf6c7708b80a2213d50e9d3e571664ba
Transfixiated <Transfixiate@hotmail.com>
2017-07-16 22:15:22 -0700

    VampireBats and BloodCloud

---
+++ Spells/Mage/Summoner/Necromancer/VampireBats.lua
@@ -0,0 +1,12 @@
+function cast(Caster, Target, MinDmg, MaxDmg, Debuff)
+  SpellDamage(Target, 2, MinDmg, MaxDmg)
+  AddSpellBonus(Target, 609880714, Debuff)
+end
+
+function tick(Caster, Target, MinDmg, MaxDmg)
+  SpellDamage(Target, 2, MinDmg, MaxDmg)
+end
+
+function remove(Caster, Target)
+  RemoveSkillBonus(Target)
+end
