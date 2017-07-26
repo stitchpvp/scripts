0f864a586084b0c4e3c67b9940f7a269829a6181
Transfixiated <Transfixiate@hotmail.com>
2017-07-25 17:55:15 -0700

    UnholyHunger

---
+++ Spells/Fighter/Crusader/Shadowknight/UnholyHunger.lua
@@ -0,0 +1,16 @@
+function cast(Caster, Target)
+    AddProc(Target, 3, 16.0)
+end
+
+function proc(Caster, Target, Type, DmgMin, DmgMax, HealMin, HealMax)
+    if Type == 3 then
+        ProcDamage(Caster, Target, "Unholy Strike", 8, DmgMin, DmgMax)
+
+        --     Heals caster for 6 - 10
+        SpellHeal("Heal", HealMin, HealMax, Caster, 2, 1)
+    end
+end
+
+function remove(Caster, Target)
+    RemoveProc(Target)
+end
