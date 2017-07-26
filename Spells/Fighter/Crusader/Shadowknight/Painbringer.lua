3205d5c95e3ad7b4ebf1b0c9ac3a969796bb1c5d
Transfixiated <Transfixiate@hotmail.com>
2017-07-25 20:24:46 -0700

    updated heals

--- Spells/Fighter/Crusader/Shadowknight/Painbringer.lua
+++ Spells/Fighter/Crusader/Shadowknight/Painbringer.lua
@@ -1,4 +1,4 @@
-function cast(Caster, Target, MinDmg, MaxDmg)
+function cast(Caster, Target, MinDmg, MaxDmg, DoTMax, HealMin, HealMax)
         SpellDamage(Target, 0, MinDmg, MaxDmg)
         SpellHeal("Heal", HealMin, HealMax, Caster, 2, 1)
 end
