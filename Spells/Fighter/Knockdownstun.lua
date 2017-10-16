38c14485361cfc446dada279c6f5d67fb8c18bdc
Jonmichael Chambers <jon@tyrbo.net>
2017-10-16 02:03:21 -0600

    Fix caps

--- Spells/Fighter/Knockdownstun.lua
+++ Spells/Fighter/Knockdownstun.lua
@@ -1,10 +1,10 @@
-function cast(caster, target)
-  AddControlEffect(target, 4)
-  Knockback(caster, target, 2500, -20, 10, 0)
+function cast(Caster, Target)
+  AddControlEffect(Target, 4)
+  Knockback(Caster, Target, 2500, -20, 10, 0)
   BlurVision(Target, 1.0)
 end
 
-function remove(caster, target)
-  RemoveControlEffect(target, 4)
+function remove(Caster, Target)
+  RemoveControlEffect(Target, 4)
   BlurVision(Target, 0.0)
 end
