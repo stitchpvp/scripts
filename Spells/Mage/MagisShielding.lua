047343e9ad58c3e98310c3b0d6681d9424390781
Jonmichael Chambers <jon@tyrbo.net>
2017-11-23 10:59:42 -0700

    Update Magi Shielding

--- Spells/Mage/MagisShielding.lua
+++ Spells/Mage/MagisShielding.lua
@@ -1,11 +1,19 @@
-function cast(Caster, Target, StaBuff, WisBuff, ResistBuff)
-  AddSpellBonus(Caster, 1, StaBuff)
-  AddSpellBonus(Caster, 204, ResistBuff)
-  AddSpellBonus(Caster, 201, ResistBuff)
-  AddSpellBonus(Caster, 202, ResistBuff)
-  AddSpellBonus(Caster, 203, ResistBuff)
+function cast(Caster, Target)
+  local Level = GetLevel(Caster)
+  local ExtraMit = 5 * (200 + 40 * Level) / (100 - 5)
+  local BaseBonus = math.ceil(Level / 2)
+
+  AddSkillBonus(Caster, GetSkillIDByName("Defense"), BaseBonus)
+  AddSkillBonus(Caster, GetSkillIDByName("Focus"), BaseBonus)
+
+  AddSpellBonus(Caster, 500, BaseBonus * 15)
+  AddSpellBonus(Caster, 201, BaseBonus)
+  AddSpellBonus(Caster, 202, BaseBonus)
+  AddSpellBonus(Caster, 203, BaseBonus)
+  AddSpellBonus(Caster, 204, ExtraMit)
 end
 
 function remove(Caster, Target)
+  RemoveSkillBonus(Caster)
   RemoveSpellBonus(Caster)
 end
