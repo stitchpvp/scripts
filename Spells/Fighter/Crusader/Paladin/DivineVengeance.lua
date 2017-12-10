6d1a6d3883d1e6753eda5399e085ba2bf4a2af9b
Jonmichael Chambers <jon@tyrbo.net>
2017-12-10 04:25:16 -0700

    Add condition to proc

--- Spells/Fighter/Crusader/Paladin/DivineVengeance.lua
+++ Spells/Fighter/Crusader/Paladin/DivineVengeance.lua
@@ -1,4 +1,7 @@
 function cast(Caster, Target, MinDmg, MaxDmg, SpellID)
   SpellDamage(Target, 7, MinDmg, MaxDmg)
-  CastSpell(Target, SpellID, 1, Caster)
+
+  if LastSpellAttackHit() then
+    CastSpell(Target, SpellID, 1, Caster)
+  end
 end
