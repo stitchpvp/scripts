8759de75ce0d5e1f525f2c47b2519d79cf3dbddf
Jonmichael Chambers <jon@tyrbo.net>
2017-12-09 19:52:40 -0700

    Add a test paladin spell

---
+++ Spells/Fighter/Crusader/Paladin/BlessedWeapon.lua
@@ -0,0 +1,7 @@
+function cast(Caster)
+  AddProc(Caster, 3, 100)
+end
+
+function proc(Caster, Target, MinDmg, MaxDmg, MinUndeadDmg, MaxUndeadDmg)
+  ProcDamage(Caster, Target, "Glorious Strike", 7, MinDmg, MaxDmg)
+end
