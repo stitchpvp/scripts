34595bf0451666dafbfc033ad207b9b46756fa0a
Jonmichael Chambers <jon@tyrbo.net>
2017-12-10 03:48:38 -0700

    Ordering

--- Spells/Fighter/Crusader/Paladin/PrayerOfHealing.lua
+++ Spells/Fighter/Crusader/Paladin/PrayerOfHealing.lua
@@ -2,6 +2,6 @@ function cast(Caster, Target, MinHealPct, MaxHealPct, Health)
   local min_heal = GetMaxHP(Caster) * (MinHealPct / 100.0)
   local max_heal = GetMaxHP(Caster) * (MaxHealPct / 100.0)
 
-  SpellHeal(Target, "Heal", min_heal, max_heal)
   AddSpellBonus(Target, 500, Health)
+  SpellHeal(Target, "Heal", min_heal, max_heal)
 end
