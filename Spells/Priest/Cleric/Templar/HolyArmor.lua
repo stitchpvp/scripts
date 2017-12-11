d569b5fc48ecdfd5e84f284ab4e3891da6f1535f
Jonmichael Chambers <jon@tyrbo.net>
2017-12-11 01:01:13 -0700

    Fix Holy Armor

--- Spells/Priest/Cleric/Templar/HolyArmor.lua
+++ Spells/Priest/Cleric/Templar/HolyArmor.lua
@@ -1,5 +1,5 @@
 function cast(Caster, Target, Health, Mitigation)
-  if GetArchetypeName(Target) ~= "Fighter" then
+  if GetArchetypeName(Target) == "Fighter" then
     AddSpellBonus(Target, 500, Health)
   else
     AddSpellBonus(Target, 204, Mitigation)
