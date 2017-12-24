f133d7c0ffa978e64a62d169dfa65ff2085e7454
Jonmichael Chambers <jon@tyrbo.net>
2017-12-24 04:45:00 -0700

    Add timer to find a victim

--- SpawnScripts/Thunderdome/GoblinWarlock.lua
+++ SpawnScripts/Thunderdome/GoblinWarlock.lua
@@ -1,9 +1,17 @@
 function spawn(NPC)
-  local victim = GetSpawn(NPC, 7720007)
-  
-  AddHate(NPC, victim, 1)
+  find_victim(NPC)
 end
 
 function respawn(NPC)
   spawn(NPC)
+end
+
+function find_victim(NPC)
+  if IsInCombat(NPC) == false then
+    local victim = GetSpawn(NPC, 7720007)
+    
+    AddHate(NPC, victim, 1)
+  end
+
+  AddTimer(NPC, 5000, "find_victim")
 end
