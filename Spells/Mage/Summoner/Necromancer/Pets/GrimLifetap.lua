7a97646fd337e41de2f239a449db40741a40c0df
Jonmichael Chambers <jon@tyrbo.net>
2017-12-31 23:25:07 -0700

    Fix pet lifetap

--- Spells/Mage/Summoner/Necromancer/Pets/GrimLifetap.lua
+++ Spells/Mage/Summoner/Necromancer/Pets/GrimLifetap.lua
@@ -1,4 +1,9 @@
 function cast(Caster, Target, DamageSpell, HealSpell)
   CastSpell(Target, DamageSpell, 1, Caster)
-  CastSpell(Caster, HealSpell, 1)
+
+  if IsPet(Caster) then
+    CastSpell(Caster, HealSpell, 1, GetOwner(Caster))
+  else
+    CastSpell(Caster, HealSpell, 1, Caster)
+  end
 end
