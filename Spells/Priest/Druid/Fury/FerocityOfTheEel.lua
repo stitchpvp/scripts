f454f50550d6d15b093411d8a045acda2072d575
Jonmichael Chambers <jon@tyrbo.net>
2018-01-03 18:31:20 -0700

    Update Ferocity of the Eel

--- Spells/Priest/Druid/Fury/FerocityOfTheEel.lua
+++ Spells/Priest/Druid/Fury/FerocityOfTheEel.lua
@@ -1,9 +1,7 @@
 function cast(Caster, Target)
   BreatheUnderwater(Caster, 1)
-  SetVision(Caster, 4)
 end
 
 function remove(Caster, Target)
   BreatheUnderwater(Caster, 0)
-  SetVision(Caster, 0)
 end
