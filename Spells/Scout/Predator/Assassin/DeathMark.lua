ae8e1b30264df5e0d20e5e46e3eedcf7a776f911
Jonmichael Chambers <jon@tyrbo.net>
2018-01-16 23:34:26 -0700

    Remove trigger count

--- Spells/Scout/Predator/Assassin/DeathMark.lua
+++ Spells/Scout/Predator/Assassin/DeathMark.lua
@@ -1,13 +1,10 @@
 function cast(Caster, Target)
-  SetSpellTriggerCount(1, 1)
-
   AddProc(Target, 16, 5)
 end
 
 function proc(Caster, Target, Type, SpellID)
   CastSpell(Caster, SpellID, 1, Target)
-
-  RemoveTriggerFromSpell()
+  CancelSpell()
 end
 
 function remove(Caster, Target)
