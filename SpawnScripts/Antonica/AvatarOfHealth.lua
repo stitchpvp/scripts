0024d31d079bae13b2ef1e23c317bf73efc870e6
Rixan <35015924+drakky@users.noreply.github.com>
2018-02-22 04:17:27 -0500

    Remove Spaces

--- SpawnScripts/Antonica/AvatarOfHealth.lua
+++ SpawnScripts/Antonica/AvatarOfHealth.lua
@@ -11,15 +11,15 @@ function hailed(NPC, Spawn)
 	else
 		if GetTempVariable(NPC, "say") ~= nil then
 		local choice = math.random(1, 3)
-	
+
 		if choice == 1 then
 		  Say(NPC, "Aaccck!!", Spawn)
-		
+
 		elseif choice == 2 then
 		  Say(NPC, "Kill it!", Spawn)
-		  
+
 		elseif choice == 3 then
-		  Say(NPC, "What is that thing?! Destroy it!", Spawn)	  
+      Say(NPC, "What is that thing?! Destroy it!", Spawn)
 		end
 	 end
   end
@@ -32,7 +32,7 @@ function Choice1(NPC, Spawn)
   SetInCombat(NPC, true)
   SpawnSet(NPC, "visual_state", 141)
  end
- 
+
 function Choice2(NPC, Spawn)
   local conversation = CreateConversation()
   StartConversation(conversation, NPC, Spawn, "That is too bad.")
