03ef19f82aa63b62d39570f50c15d9fc1abae4a3
Rixan <35015924+drakky@users.noreply.github.com>
2018-02-22 04:13:31 -0500

    Avatar of Health changes

--- SpawnScripts/Antonica/Grummus.lua
+++ SpawnScripts/Antonica/Grummus.lua
@@ -5,20 +5,27 @@ function spawn(NPC)
   AddTimer(NPC, 45000, "battleweary") --[[change back to 390000 when done testing]]
 end
 
-function battleweary(NPC)
+function battleweary(NPC, Spawn)
   if IsInCombat(NPC) == false then
   return
 end
   Say(NPC, "I see you grow tired from my onslaught, it won't be long now before victory is mine!")
   CastSpell(NPC, 1002183, 1) --[[Aura of Confidence]]
+  CastSpell(Spawn, 1002186, 1, NPC) --[Battleweary]]
 end
 
 function death(NPC)
   local Spawn = GetSpawn(NPC, 2520010)
+  SetTempVariable(Spawn, "say", nil)
   Say(Spawn, "You have engaged in a struggle without cause and although you prevailed, there was little reason to entertain the folly of disease... Only with inner peace and discipline can true, lasting health thrive.")
+  SpawnSet(NPC, "visual_state", 0)
   Despawn(Spawn, 10000)
 end
 
-function CombatReset(NPC)
+function CombatReset(NPC, Spawn)
+  local Spawn = GetSpawn(NPC, 2520010)
+  SetTempVariable(Spawn, "grummus_spawned", nil)
+  SetTempVariable(Spawn, "say", nil)
+  SpawnSet(Spawn, "visual_state", 0)
   Despawn(NPC)
 end
