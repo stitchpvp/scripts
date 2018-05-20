ba076d41327b4bc80b43ada82f41952563726f1c
Jonmichael Chambers <jon@tyrbo.net>
2018-05-20 02:13:41 -0600

    Update Cataclysm

--- Spells/Mage/Sorcerer/Warlock/Cataclysm.lua
+++ Spells/Mage/Sorcerer/Warlock/Cataclysm.lua
@@ -2,6 +2,6 @@ function cast(Caster, Target, MinDmg, MaxDmg)
   SpellDamage(Target, 9, MinDmg, MaxDmg)
 end
 
-function tick(Caster, Target, MinDmg, MaxDmg)
-  SpellDamage(Target, 9, MinDmg, MaxDmg)
+function tick(Caster, Target, MinDmg, MaxDmg, MinTick, MaxTick)
+  SpellDamage(Target, 9, MinTick, MaxTick)
 end
