246ab37a27bd5bfa6d2368a24c16f63d164f26ef
Jonmichael Chambers <jon@tyrbo.net>
2018-05-28 19:05:22 -0600

    Update Shadow Slip

--- Spells/Scout/Rogue/Brigand/ShadowSlip.lua
+++ Spells/Scout/Rogue/Brigand/ShadowSlip.lua
@@ -1,7 +1,7 @@
 function cast(Caster, Target, Dehate)
   AddHate(Caster, Target, Dehate)
 
-  CastSpell(Caster, 1001214, 1)
+  CastSpell(Caster, 232860311, 1)
 
   if IsPlayer(Target) then
     SetTarget(Target, 0xFFFFFFFF)
