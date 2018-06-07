7c6d8765d72055de0f1933f8a450c078038b32e1
Jonmichael Chambers <jon@tyrbo.net>
2018-06-07 07:53:06 -0600

    More spell things

--- Spells/NPC/Bosses/Commonlands/Golem/DustRift.lua
+++ Spells/NPC/Bosses/Commonlands/Golem/DustRift.lua
@@ -1,3 +1,4 @@
-function cast(Caster, Target)
-  Knockback(Caster, Target, -100, 45)
+function cast(Caster, Target, MinDmg, MaxDmg)
+  SpellDamage(Target, 5, MinDmg, MaxDmg)
+  Knockback(Caster, Target, -100, -45)
 end
