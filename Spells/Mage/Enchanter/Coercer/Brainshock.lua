505007dadef022f4510e19c96ed640352b10da32
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-08 01:12:32 -0400

    Add power gain to caster

--- Spells/Mage/Enchanter/Coercer/Brainshock.lua
+++ Spells/Mage/Enchanter/Coercer/Brainshock.lua
@@ -1,7 +1,9 @@
-function cast(Caster, Target, MinDmg, MaxDmg)
+function cast(Caster, Target, MinDmg, MaxDmg, PowerAmount)
   SpellDamage(Target, 6, MinDmg, MaxDmg)
+  SpellHeal(Caster, "Power", PowerAmount, PowerAmount)
 end
 
-function tick(Caster, Target, MinDmg, MaxDmg)
+function tick(Caster, Target, MinDmg, MaxDmg, PowerAmount)
   SpellDamage(Target, 6, MinDmg, MaxDmg)
+  SpellHeal(Caster, "Power", PowerAmount, PowerAmount)
 end
