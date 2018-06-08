307e9958d9e1f429fea9fe4f242fd8bf4ebcf2bc
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-08 13:18:42 -0400

    Added snare spells to Necromancer's Dooming Darkness

--- Spells/Mage/Summoner/Necromancer/DoomingDarkness.lua
+++ Spells/Mage/Summoner/Necromancer/DoomingDarkness.lua
@@ -1,6 +1,7 @@
 function cast(Caster, Target, MinDmg, MaxDmg)
   SpellDamage(Target, 8, MinDmg, MaxDmg)
   Interrupt(Caster, Target)
+  CastSpell(Target, 112338607, 1, Caster)
 end
 
 function tick(Caster, Target, MinDmg, MaxDmg)
