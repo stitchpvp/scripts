fe3ca410c7bcff756522b70c8ead43c5ec2d0f96
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-13 05:38:48 -0400

    Fix Glorious Strike proc on Divine Vengeance

--- Spells/Fighter/Crusader/Paladin/DivineVengeance.lua
+++ Spells/Fighter/Crusader/Paladin/DivineVengeance.lua
@@ -1,7 +1,7 @@
-function cast(Caster, Target, MinDmg, MaxDmg, SpellID)
+function cast(Caster, Target, MinDmg, MaxDmg)
   SpellDamage(Target, 7, MinDmg, MaxDmg)
 
   if LastSpellAttackHit() then
-    CastSpell(Target, SpellID, 1, Caster)
+    CastSpell(Target, 96694323, 1, Caster)
   end
 end
