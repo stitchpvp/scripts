b5a4caa065e8081cb396a43798ddec94e136a933
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-14 22:50:15 -0400

    Holy Circle Heal adjustment

--- Spells/Fighter/Crusader/Paladin/HolyCircleHeal.lua
+++ Spells/Fighter/Crusader/Paladin/HolyCircleHeal.lua
@@ -1,6 +1,3 @@
-function cast(Caster, Target, MinHealPct, MaxHealPct)
-  local min_heal = GetMaxHP(Caster) * (MinHealPct / 100.0)
-  local max_heal = GetMaxHP(Caster) * (MaxHealPct / 100.0)
-
-  SpellHeal(Target, "Heal", min_heal, max_heal)
+function cast(Caster, Target, MinHeal, MaxHeal)
+  SpellHeal(Target, "Heal", MinHeal, MaxHeal)
 end
