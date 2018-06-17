45ff8568f3012f52adac8d80c416663fe0bacabe
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-17 04:20:16 -0400

    Adding BlurVision to SmokeBomb

--- Spells/Scout/Predator/Assassin/SmokeBomb.lua
+++ Spells/Scout/Predator/Assassin/SmokeBomb.lua
@@ -1,8 +1,13 @@
 function cast(Caster, Target, Dehate)
   AddHate(Caster, Target, Dehate)
   CastSpell(Caster, 2903996, 1)
+  BlurVision(Target, 10.0)
 
   if IsPlayer(Target) then
     SetTarget(Target, 0xFFFFFFFF)
   end
+end
+
+function remove(Caster, Target)
+  BlurVision(Target, 0.0)
 end
