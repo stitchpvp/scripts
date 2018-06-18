d302fa2b35af6ea99b936a54c1f2834375fb1a1e
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-18 03:53:23 -0400

    Fixing Sandstorm v2

--- Spells/Priest/Druid/Warden/DustStorm.lua
+++ Spells/Priest/Druid/Warden/DustStorm.lua
@@ -1,8 +1,8 @@
 function cast(Caster, Target)
-  AddControlEffect(Target, 4)
-  Knockback(Caster, Target, -20, 10)
+  AddControlEffect(Caster, 4)
+  Knockback(Caster, Caster, -20, 10)
 end
 
 function remove(Caster, Target)
-  RemoveControlEffect(Target, 4)
+  RemoveControlEffect(Caster, 4)
 end
