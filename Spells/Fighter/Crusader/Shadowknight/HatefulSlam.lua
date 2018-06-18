c5138d351183852df34dd1a5f5b04e040f20f8ec
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-18 05:03:11 -0400

    SK's Hateful Slam Knockdown 1.5 seconds

--- Spells/Fighter/Crusader/Shadowknight/HatefulSlam.lua
+++ Spells/Fighter/Crusader/Shadowknight/HatefulSlam.lua
@@ -2,7 +2,7 @@ function cast(Caster, Target, MinDmg, MaxDmg, HateAmt)
   AddHate(Caster, Target, HateAmt, 1)
   SpellDamage(Target, 1, MinDmg, MaxDmg)
 
-  CastSpell(Target, 68480171, 1, Caster)
+  CastSpell(Target, 181890697, 1, Caster)
 
   if IsPlayer(Target) then
     SetTarget(Target, Caster)
