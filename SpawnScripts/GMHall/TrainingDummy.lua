66985224bef3f1e79a8f054fe9742a51be84223d
Jonmichael Chambers <jon@tyrbo.net>
2018-06-19 17:50:49 -0600

    Don't hardcode HP

--- SpawnScripts/GMHall/TrainingDummy.lua
+++ SpawnScripts/GMHall/TrainingDummy.lua
@@ -7,28 +7,14 @@
 --]]
 
 function spawn(NPC)
-	-- set the calls to the ai to 10 mins as there is no ai
 	SetBrainTick(NPC, 600000)
 	SetLuaBrain(NPC)
-
-	-- give the spawn a crap load of hp so we can't one hit kill
-	SetHP(NPC, 1000000)
-end
-
-function hailed(NPC, Spawn)
-	Say(NPC, GetHP(NPC))
 end
 
 function respawn(NPC)
-spawn(NPC)
+  spawn(NPC)
 end
 
 function Think(NPC)
-	-- no ai so won't attack
 	return
-end
-
-function healthchanged(NPC, Spawn)
-	-- insta heal so should be impossible to kill without the /kill command
-	SetHP(NPC, GetMaxHP(NPC))
 end
