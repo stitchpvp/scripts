964491719202568952e42943861ed9e059f04812
Jonmichael Chambers <jon@tyrbo.net>
2018-06-19 19:35:55 -0600

    Spawn adds on Rumbler

--- SpawnScripts/Antonica/TheWindstalkerRumbler.lua
+++ SpawnScripts/Antonica/TheWindstalkerRumbler.lua
@@ -1,5 +1,20 @@
-function attacked(NPC)
+local BURROWING_RUMBLER = 7720034
+
+local SPAWN_LOCATIONS = {
+  {-788.74, -31.21, -40.74},
+  {-764.08, -31.20, -35.92},
+  {-754.49, -30.20, -56.50},
+  {-756.30, -29.23, -72.08},
+  {-774.54, -28.97, -91.92},
+  {-801.27, -28.14, -88.32}
+}
+
+function spawn(NPC)
   SetTempVariable(NPC, "target_pct", 90)
+end
+
+function CombatReset(NPC)
+  spawn(NPC)
 end
 
 function healthchanged(NPC, Target)
@@ -7,7 +22,15 @@ function healthchanged(NPC, Target)
   local percent = (GetHP(NPC) / GetMaxHP(NPC)) * 100
 
   if percent <= target_pct then
-    Say(NPC, "Casting a spell!")
+    -- Say(NPC, "Casting a spell!")
+
+    for i = 1, 3 do
+      local loc = SPAWN_LOCATIONS[math.random(#SPAWN_LOCATIONS)]
+
+      local RumblerAdd = SpawnMob(GetZone(NPC), BURROWING_RUMBLER, false, loc[1], loc[2], loc[3])
+
+      AddHate(Target, RumblerAdd, 1)
+    end
 
     if target_pct > 10 then
       SetTempVariable(NPC, "target_pct", target_pct - 10)
