aae8516553cb8956391ce0e8dd3a591c7b9dd55f
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-20 15:34:55 -0400

    Poisons proc changes

--- Spells/Procs/HemotoxinPoison.lua
+++ Spells/Procs/HemotoxinPoison.lua
@@ -1,15 +1,20 @@
 function cast(Caster, Target)
+  local weapon = GetEquippedItemBySlot(Caster, 0)
+  local proc_chance = GetProcPercentageForWeapon(weapon, 5.0)
+  
   SetSpellTriggerCount(100, 1)
-  AddProc(Target, 3, 50)
+  AddProc(Target, 3, proc_chance)
 end
 
 function proc(Caster, Target, ProcType)
-  local spell_caster = GetCaster()
-  if HasSpellEffect(Target, 217660483) then
-    return
-  else
-    CastSpell(Caster, 217660483, 1, spell_caster)
-    RemoveTriggerFromSpell()
+  if GetSpellTriggerCount() > 0 then
+    local spell_caster = GetCaster()
+    if HasSpellEffect(Target, 217660483) then
+	  return
+    else
+	  RemoveTriggerFromSpell()
+      CastSpell(Caster, 217660483, 1, spell_caster)
+    end
   end
 end
 
