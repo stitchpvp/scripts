1815256efac09fc1bfae827efbddd241e38a6485
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-20 06:28:20 -0400

    Faith Strike change

--- Spells/Fighter/Crusader/Paladin/FaithStrike.lua
+++ Spells/Fighter/Crusader/Paladin/FaithStrike.lua
@@ -1,7 +1,4 @@
-function cast(Caster, Target, MinDmg, MaxDmg, MinHealPct, MaxHealPct)
-  local min_heal = GetMaxHP(Caster) * (MinHealPct / 100.0)
-  local max_heal = GetMaxHP(Caster) * (MaxHealPct / 100.0)
-
+function cast(Caster, Target, MinDmg, MaxDmg, MinHeal, MaxHeal)
   SpellDamage(Target, 7, MinDmg, MaxDmg)
-  SpellHeal(Caster, "Heal", min_heal, max_heal)
+  SpellHeal(Caster, "Heal", MinHeal, MaxHeal)
 end
