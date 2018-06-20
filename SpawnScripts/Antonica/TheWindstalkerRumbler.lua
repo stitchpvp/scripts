cfc402bb94f19249618e5fa87f91d6bc8b2825c6
Jonmichael Chambers <jon@tyrbo.net>
2018-06-19 19:09:04 -0600

    dumb

--- SpawnScripts/Antonica/TheWindstalkerRumbler.lua
+++ SpawnScripts/Antonica/TheWindstalkerRumbler.lua
@@ -4,7 +4,7 @@ end
 
 function healthchanged(NPC, Target)
   local target_pct = GetTempVariable(NPC, "target_pct")
-  local percent = GetCurrentHP(NPC) / GetMaxHP(NPC)
+  local percent = GetHP(NPC) / GetMaxHP(NPC)
 
   if percent <= target_pct then
     Say(NPC, "Casting a spell!")
