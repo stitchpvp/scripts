9303d54c29d3933b9606e2ad33520733642cc68e
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-20 15:32:42 -0400

    Poisons proc changes

--- Spells/Procs/CausticPoison.lua
+++ Spells/Procs/CausticPoison.lua
@@ -1,12 +1,19 @@
 function cast(Caster, Target)
+  local weapon = GetEquippedItemBySlot(Caster, 0)
+  local proc_chance = GetProcPercentageForWeapon(weapon, 5.0)
+
   SetSpellTriggerCount(100, 1)
-  AddProc(Target, 3, 50)
+  AddProc(Target, 3, proc_chance)
 end
 
 function proc(Caster, Target, ProcType, MinDmg, MaxDmg)
-  local spell_caster = GetCaster()
-  ProcDamage(spell_caster, Target, "Caustic Poison", 9, MinDmg, MaxDmg)
-  RemoveTriggerFromSpell()
+  if GetSpellTriggerCount() > 0 then
+    local spell_caster = GetCaster()
+  
+    RemoveTriggerFromSpell()
+	
+    ProcDamage(spell_caster, Target, "Caustic Poison", 9, MinDmg, MaxDmg)
+  end
 end
 
 function remove(Caster, Target)
