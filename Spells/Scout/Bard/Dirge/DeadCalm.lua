900fb610d47df2949e56494c9dfc120e0c71bb0d
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-22 19:36:39 -0400

    Dead Calm fix

--- Spells/Scout/Bard/Dirge/DeadCalm.lua
+++ Spells/Scout/Bard/Dirge/DeadCalm.lua
@@ -1,13 +1,11 @@
-function cast(Caster, Target)
-    AddProc(Target, 1, 12.0)
+function cast(Caster, Target, MinDmg, MaxDmg)
+  AddProc(Target, 1, 12.0)
 end
 
-function proc(Caster, Target, Type, DmgMin, DmgMax)
-    if Type == 3 then
-        ProcDamage(Caster, Target, "Crypt's Revenge", 8, DmgMin, DmgMax)
-    end
+function proc(Caster, Target, Type, MinDmg, MaxDmg)
+  ProcDamage(Caster, Target, "Crypt's Revenge", 8, MinDmg, MaxDmg)
 end
 
 function remove(Caster, Target)
-    RemoveProc(Target)
+  RemoveProc(Target)
 end
