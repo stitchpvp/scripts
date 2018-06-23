17c29cf53d7b5f8c43918983db773a06171b6425
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-23 01:35:01 -0400

    Ring of Fire, round 2

--- Spells/Priest/Druid/Fury/RingOfFire.lua
+++ Spells/Priest/Druid/Fury/RingOfFire.lua
@@ -4,15 +4,4 @@ function cast(Caster, Target, PetID)
   local z = GetZ(Caster)
 
   SummonDumbFirePet(Caster, Target, PetID, x, y, z)
-  
-  local pet = GetSpawn(Caster, PetID)
-  AddControlEffect(pet, 3)
-  AddControlEffect(pet, 5)
-end
-
-function remove(Caster, Target, PetID)
-  local pet = GetSpawn(Caster, PetID)
-
-  RemoveControlEffect(pet, 3)
-  RemoveControlEffect(pet, 5)
 end
--- Spells/Priest/Druid/Fury/RingOfFireDumbfireTick.lua
+++ Spells/Priest/Druid/Fury/RingOfFireDumbfireTick.lua
@@ -1,7 +1,14 @@
 function cast(Caster, Target, MinDmg, MaxDmg)
+  AddControlEffect(Caster, 3)
+  AddControlEffect(Caster, 5)
   SpellDamage(Target, 3, MinDmg, MaxDmg)
 end
 
 function tick(Caster, Target, MinDmg, MaxDmg)
   SpellDamage(Target, 3, MinDmg, MaxDmg)
+end
+
+function remove(Caster, Target)
+  RemoveControlEffect(Caster, 3)
+  RemoveControlEffect(Caster, 5)
 end
