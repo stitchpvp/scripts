854437746d2263c636d4be00de795da7564d59ca
Jonmichael Chambers <jon@tyrbo.net>
2018-06-23 19:12:25 -0600

    Seriously.

--- SpawnScripts/WeakenTrainingDummy.lua
+++ SpawnScripts/WeakenTrainingDummy.lua
@@ -1,4 +1,15 @@
 function spawn(NPC)
   SetMaxHPBase(NPC, 1500)
   SetHP(NPC, 1500)
+
+	SetBrainTick(NPC, 600000)
+	SetLuaBrain(NPC)
+end
+
+function respawn(NPC)
+  spawn(NPC)
+end
+
+function Think(NPC)
+	return
 end
