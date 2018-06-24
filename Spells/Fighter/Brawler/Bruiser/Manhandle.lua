0419ca941b36eb1dcad3d5642b740c950c4076b2
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-24 11:46:21 -0400

    ManHandle

--- Spells/Fighter/Brawler/Bruiser/Manhandle.lua
+++ Spells/Fighter/Brawler/Bruiser/Manhandle.lua
@@ -8,7 +8,7 @@ end
 function proc(Caster, Target, ProcType, MinDmg, MaxDmg, MinHate, MaxHate)
   local DamageType = GetWeaponDamageType(Caster)
   
-  SpellDamage(Target, DamageType, MinDmg, MaxDmg)
+  ProcDamage(Caster, Target, "Roughhousing", DamageType, MinDmg, MaxDmg)
   AddHate(Caster, Target, math.random(MinHate, MaxHate), 1)
 end
 
