6ca6fec3aa95d83d51b7e04c770a9d40415ae274
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-24 23:14:11 -0400

    Fix ordering in Enraged Vigor

--- Spells/Fighter/Warrior/Berserker/EnragedVigor.lua
+++ Spells/Fighter/Warrior/Berserker/EnragedVigor.lua
@@ -5,14 +5,13 @@ end
 function proc(Caster, Target, ProcType, MinDmg, MaxDmg)
   local DamageType = GetWeaponDamageType(Caster)
 
-  ProcDamage(Caster, Target, "Enraged Vigor", DamageType, MinDmg, MaxDmg)
-
   local percentage = (GetHP(Caster) / GetMaxHP(Caster)) * 100
   if percentage <= 30 then
     MinDmg = MinDmg * 2
     MaxDmg = MaxDmg * 2
   end
-
+  
+  ProcDamage(Caster, Target, "Enraged Vigor", DamageType, MinDmg, MaxDmg)
   ProcHeal(Caster, Caster, "Enraged Vigor", "Heal", MinDmg, MaxDmg)
 end
 
