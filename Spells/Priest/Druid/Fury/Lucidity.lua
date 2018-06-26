5699f1b2dc49a12e509e20e8ac8cbf95a150aeb3
Rixan <35015924+drakky@users.noreply.github.com>
2018-06-25 21:37:47 -0400

    Lucidity change

--- Spells/Priest/Druid/Fury/Lucidity.lua
+++ Spells/Priest/Druid/Fury/Lucidity.lua
@@ -1,5 +1,5 @@
-function cast(Caster, Target, Sta, Int)
-  AddSpellBonus(Target, 1, Sta)
+function cast(Caster, Target, MaxHealth, Int)
+  AddSpellBonus(Target, 500, MaxHealth)
   AddSpellBonus(Target, 4, Int)
 end
 
