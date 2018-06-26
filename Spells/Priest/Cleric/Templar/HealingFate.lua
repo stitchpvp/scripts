0db9118e4d9e5011feea064df3f04dd7f72a2e2e
Jonmichael Chambers <jon@tyrbo.net>
2018-06-25 23:06:57 -0600

    Fix trigger count on wrong spell

--- Spells/Priest/Cleric/Templar/HealingFate.lua
+++ Spells/Priest/Cleric/Templar/HealingFate.lua
@@ -1,20 +1,25 @@
 function cast(Caster, Target)
   local var_name = "Spell_" .. GetName(Caster) .. "_HealingFate"
 
+  SetSpellTriggerCount(1, 1)
+
   AddProc(Target, 2, 100)
   SetTempVariable(Target, var_name, GetHP(Target))
 end
 
 function proc(Caster, Target, Type, SpellID)
-  local spell_caster = GetCaster()
-  local var_name = "Spell_" .. GetName(spell_caster) .. "_HealingFate"
+  if GetSpellTriggerCount() > 0 then
+    local spell_caster = GetCaster()
+    local var_name = "Spell_" .. GetName(spell_caster) .. "_HealingFate"
 
-  local start_hp = GetTempVariable(Caster, var_name)
-  local target_hp = start_hp - (start_hp * 0.2)
+    local start_hp = GetTempVariable(Caster, var_name)
+    local target_hp = start_hp - (start_hp * 0.2)
 
-  if GetHP(Caster) <= target_hp then
-    CastSpell(spell_caster, SpellID, 1, spell_caster, true)
-    CancelSpell()
+    if GetHP(Caster) <= target_hp then
+      CastSpell(spell_caster, SpellID, 1, spell_caster, true)
+
+      RemoveTriggerFromSpell()
+    end
   end
 end
 
--- Spells/Priest/Cleric/Templar/MarkOfDivinity.lua
+++ Spells/Priest/Cleric/Templar/MarkOfDivinity.lua
@@ -1,18 +1,12 @@
 function cast(Caster, Target, Arcane)
-  SetSpellTriggerCount(1, 1)
-
   AddSpellBonus(Target, 203, Arcane)
   AddProc(Target, 16, 20)
 end
 
 function proc(Caster, Target, Type, Arcane, SpellID)
-  if GetSpellTriggerCount() > 0 then
-    local spell_caster = GetCaster()
+  local spell_caster = GetCaster()
 
-    RemoveTriggerFromSpell()
-
-    CastSpell(Target, SpellID, 1, spell_caster)
-  end
+  CastSpell(Target, SpellID, 1, spell_caster)
 end
 
 function remove(Caster, Target)
