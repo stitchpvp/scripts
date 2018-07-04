9ad4e76dbe2d9cdd2df2c5297ff712c6575a446e
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-04 19:06:22 -0400

    Reaver fix

--- Spells/Fighter/Crusader/Shadowknight/Reaver.lua
+++ Spells/Fighter/Crusader/Shadowknight/Reaver.lua
@@ -1,11 +1,12 @@
 function cast(Caster, Target, Heal)
-  local heal_pct = GetMaxHP(Caster) * Heal
 
   AddProc(Caster, 5, 100)
 end
 
 function proc(Caster, Target, Type, Heal)
-  ProcHeal(Caster, Caster, "Reaver's Mania", "Heal", Heal)
+  local heal_pct = GetMaxHP(Caster) * Heal
+
+  ProcHeal(Caster, Caster, "Reaver's Mania", "Heal", heal_pct)
 end
 
 function remove(Caster, Target)
