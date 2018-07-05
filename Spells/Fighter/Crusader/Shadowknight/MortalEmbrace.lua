1579e814401271d09cb26d1f11639d2651736399
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-05 04:36:00 -0400

    Mortal Embrace change

--- Spells/Fighter/Crusader/Shadowknight/MortalEmbrace.lua
+++ Spells/Fighter/Crusader/Shadowknight/MortalEmbrace.lua
@@ -1,17 +1,21 @@
 function cast(Caster, Target, Chance)
-  SetServerControlFlag(Target, 5, 1, 1);
 
   local roll = math.random(1, 100);
   if roll <= Chance then
-    ClearHate(Target)
-    SendMessage(Caster, "Your feign death succeeded", "yellow")
-    SendMessage(Target, "Your feign death succeeded", "yellow")
+	SendMessage(Caster, "Your feign death succeeded.", "yellow")
+	SendMessage(Target, "Your feign death succeeded.", "yellow")
+	AddControlEffect(Target, 16)
+	ClearHate(Target)
+
+	if IsPlayer(Target) then
+	  SetTarget(Target, 0xFFFFFFFF)
+	end
   else
-    SendMessage(Caster, "Your feign death attempt failed", "yellow")
-    SendMessage(Target, "Your feign death attempt failed", "yellow")
+    SendMessage(Caster, "Your feign death attempt failed.", "yellow")
+    SendMessage(Target, "Your feign death attempt failed.", "yellow")
   end
 end
 
 function remove(Caster, Target)
-  SetServerControlFlag(Target, 5, 1, 0);
+  RemoveControlEffect(Target, 16)
 end
