6344324300e1d12c0b3b8a20fd38987268423944
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-09 20:09:12 -0400

    Clears target's window when used

--- Spells/Fighter/Brawler/FeignDeath.lua
+++ Spells/Fighter/Brawler/FeignDeath.lua
@@ -1,6 +1,11 @@
 function cast(Caster, Target, Chance)
   AddControlEffect(Caster, 16)
 
+  local player_target = GetTarget(Caster)
+    if IsPlayer(player_target) then
+      SetTarget(player_target, 0xFFFFFFFF)
+    end
+	
   local roll = math.random(1, 100);
   if roll <= Chance then
 	SendMessage(Caster, "Your feign death succeeded.", "yellow")
