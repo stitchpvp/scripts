8941deaa3309d134b6951f059a64da32109ce698
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-13 19:07:24 -0400

    Disarming Grin, add wisdom to Harl's

--- Spells/Scout/Bard/Dirge/HarlsRousingTune.lua
+++ Spells/Scout/Bard/Dirge/HarlsRousingTune.lua
@@ -1,9 +1,10 @@
 function cast(Caster, Target, StatAmount, AvoidanceAmt)
-    AddSpellBonus(Target, 0, StatAmount)
-    AddSpellBonus(Target, 2, StatAmount)
-    AddSpellBonus(Target, 696, AvoidanceAmt)
+  AddSpellBonus(Target, 0, StatAmount)
+  AddSpellBonus(Target, 2, StatAmount)
+  AddSpellBonus(Target, 3, StatAmount)
+  AddSpellBonus(Target, 696, AvoidanceAmt)
 end
 
 function remove(Caster, Target)
-    RemoveSpellBonus(Target)
+  RemoveSpellBonus(Target)
 end
