1fe376ac40494cca840bc5d71760eab1b7a5dc28
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-15 17:46:39 -0400

    Reverted Smoke Bomb

--- Spells/Scout/Predator/Assassin/SmokeBomb.lua
+++ Spells/Scout/Predator/Assassin/SmokeBomb.lua
@@ -4,21 +4,8 @@ function cast(Caster, Target, Dehate)
   BlurVision(Target, 20.0)
   Interrupt(Caster, Target)
 
-  local find_target = GetTarget(Caster)
-
-  if find_target ~= nil then
-    if not HasGroup(find_target) then
-      SetTarget(find_target, 0xFFFFFFFF)
-    else
-      local group_target = GetGroup(find_target)
-      for _, players in ipairs(group_target) do
-	    if IsPlayer(players) then
-	      SetTarget(players, 0xFFFFFFFF)
-	    end
-      end
-    end
-  else
-    return
+  if IsPlayer(Target) then
+	SetTarget(Target, 0xFFFFFFFF)
   end
 end
 
