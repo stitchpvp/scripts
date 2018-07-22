904228fa47ff6eaa32012430148779cbf3fada47
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-22 00:49:53 -0400

    Mount spell

---
+++ Spells/Mount.lua
@@ -0,0 +1,11 @@
+function cast(Caster, Target, Mitigation, Speed, MountID)
+  AddSpellBonus(Caster, 201, Mitigation)
+  AddSpellBonus(Caster, 202, Mitigation)
+  AddSpellBonus(Caster, 203, Mitigation)
+  SetSpeed(Caster, Speed)
+  SetMount(Caster, MountID)
+end
+
+function remove(Caster, Target)
+  RemoveSpellBonus(Caster)
+end
