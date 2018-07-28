70fb436f755222b07cee2d659726fd89f14482c6
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-28 03:03:13 -0400

    Ignore Pain change

--- Spells/Fighter/Brawler/Bruiser/IgnorePain.lua
+++ Spells/Fighter/Brawler/Bruiser/IgnorePain.lua
@@ -1,3 +1,7 @@
-function cast(Caster, Target, MinHeal, MaxHeal)
-  SpellHeal(Caster, "Heal", MinHeal, MaxHeal)
+function cast(Caster, Target, MinHeal, MaxHeal, Percentage)
+  local base_hp = GetMaxHP(Caster) * Percentage
+  local random = math.random(MinHeal, MaxHeal)
+  local heal = base_hp + random
+
+  SpellHeal(Target, "Heal", heal)
 end
