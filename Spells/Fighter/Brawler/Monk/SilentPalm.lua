4e38dee8f6dc20a70993601d88de783f71f587a3
Rixan <35015924+drakky@users.noreply.github.com>
2018-07-27 23:40:44 -0400

    Silent Palm

---
+++ Spells/Fighter/Brawler/Monk/SilentPalm.lua
@@ -0,0 +1,11 @@
+function cast(Caster, Target, MinDmg, MaxDmg)
+  AddControlEffect(Target, 2)
+
+  local DamageType = GetWeaponDamageType(Caster)
+  
+  SpellDamage(Target, DamageType, MinDmg, MaxDmg)
+end
+
+function remove(Caster, Target)
+  RemoveControlEffect(Target, 2)
+end
