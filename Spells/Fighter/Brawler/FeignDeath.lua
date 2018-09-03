fc134d458e2d299fc72bdd2701e2062f859b57d0
Jonmichael Chambers <jon@tyrbo.net>
2018-09-03 08:57:26 -0600

    Update FD

--- Spells/Fighter/Brawler/FeignDeath.lua
+++ Spells/Fighter/Brawler/FeignDeath.lua
@@ -1,16 +1,17 @@
 function cast(Caster, Target, Chance)
   AddControlEffect(Caster, 16)
 
-  local player_target = GetTarget(Caster)
-    if IsPlayer(player_target) then
-      SetTarget(player_target, 0xFFFFFFFF)
-    end
-	
-  local roll = math.random(1, 100);
+  local roll = math.random(1, 100)
+
   if roll <= Chance then
-	SendMessage(Caster, "Your feign death succeeded.", "yellow")
-	ClearHate(Caster)
+    SendMessage(Caster, "Your feign death succeeded.", "yellow")
+    SetIgnoredByMobs(Caster, true)
+    ClearHate(Caster)
   else
     SendMessage(Caster, "Your feign death attempt failed.", "yellow")
   end
+end
+
+function remove(Caster)
+  SetIgnoredByMobs(Caster, false)
 end
--- Spells/Fighter/Crusader/Shadowknight/MortalEmbrace.lua
+++ Spells/Fighter/Crusader/Shadowknight/MortalEmbrace.lua
@@ -2,15 +2,12 @@ function cast(Caster, Target, Chance)
   AddControlEffect(Target, 16)
 
   local roll = math.random(1, 100)
-  if roll <= Chance then
-	SendMessage(Caster, "Your feign death succeeded.", "yellow")
-	SendMessage(Target, "Your feign death succeeded.", "yellow")
-	ClearHate(Target)
 
-	local player_target = GetTarget(Target)
-	if IsPlayer(player_target) then
-	  SetTarget(player_target, 0xFFFFFFFF)
-	end
+  if roll <= Chance then
+    SendMessage(Caster, "Your feign death succeeded.", "yellow")
+    SendMessage(Target, "Your feign death succeeded.", "yellow")
+    SetIgnoredByMobs(Target, true)
+    ClearHate(Target)
   else
     SendMessage(Caster, "Your feign death attempt failed.", "yellow")
     SendMessage(Target, "Your feign death attempt failed.", "yellow")
@@ -18,5 +15,6 @@ function cast(Caster, Target, Chance)
 end
 
 function remove(Caster, Target)
+  SetIgnoredByMobs(Target, false)
   RemoveControlEffect(Target, 16)
 end
