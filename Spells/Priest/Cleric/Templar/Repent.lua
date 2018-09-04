9df620e77250566284a14b080fc4ff595194c9b8
Jonmichael Chambers <jon@tyrbo.net>
2018-09-03 22:50:03 -0600

    Adds heal to Repent

--- Spells/Priest/Cleric/Templar/Repent.lua
+++ Spells/Priest/Cleric/Templar/Repent.lua
@@ -1,12 +1,17 @@
 function cast(Caster, Target, WardAmt)
+  AddProc(Target, 2, 100)
   AddWard(Target, WardAmt, 0, 0)
 end
 
+function proc(Victim, Attacker)
+  local heal = GetLastDamageWarded(Victim)
+
+  if heal > 0 then
+    ProcHeal(GetCaster(), Victim, "Repent", "Heal", heal, heal)
+  end
+end
+
 function remove(Caster, Target)
-  --local heal = GetWardAmountLeft()
-  
+  RemoveProc(Target)
   RemoveWard(Target)
-  --if heal > 0 then
-    --SpellHeal(Target, "Heal", heal)
-  --end
 end
