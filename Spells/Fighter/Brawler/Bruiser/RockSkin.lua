5ec00b31bbf7eb59a982b4590e24a6199f09f8d3
Rixan <35015924+drakky@users.noreply.github.com>
2018-09-30 03:09:59 -0400

    Add snare to Rock Skin. Fix Sonic Punch teleport to allow melee autoattack

--- Spells/Fighter/Brawler/Bruiser/RockSkin.lua
+++ Spells/Fighter/Brawler/Bruiser/RockSkin.lua
@@ -1,10 +1,13 @@
-function cast(Caster, Target, PhysMitigation, OtherMitigation)
+function cast(Caster, Target, PhysMitigation, OtherMitigation, SelfSnare)
   AddSpellBonus(Caster, 204, PhysMitigation)
   AddSpellBonus(Caster, 201, OtherMitigation)
   AddSpellBonus(Caster, 202, OtherMitigation)
   AddSpellBonus(Caster, 203, OtherMitigation)
+  SetSpellSnareValue(SelfSnare, Caster)
+  AddControlEffect(Caster, 11)
 end
 
 function remove(Caster, Target)
+  RemoveControlEffect(Caster, 11)
   RemoveSpellBonus(Caster)
 end
--- Spells/Fighter/Brawler/Bruiser/ShoulderCharge.lua
+++ Spells/Fighter/Brawler/Bruiser/ShoulderCharge.lua
@@ -1,7 +1,7 @@
 function cast(Caster, Target, MinDmg, MaxDmg)
   local DamageType = GetWeaponDamageType(Caster)
 
-  SetPosition(Caster, GetX(Target), GetY(Target), GetZ(Target))
+  SetPosition(Caster, GetX(Target) + 1, GetY(Target), GetZ(Target) + 1)
   SpellDamage(Target, DamageType, MinDmg, MaxDmg)
   Interrupt(Caster, Target)
 end
