6d4ee15e1861db62f9b5b32cbda6b7e216dd5b0a
Rixan <35015924+drakky@users.noreply.github.com>
2018-10-01 06:32:48 -0400

    Additional proc types to Blade of Peace (because of spell cast wording)

--- Spells/Items/HastenedCasting.lua
+++ Spells/Items/HastenedCasting.lua
@@ -2,6 +2,8 @@ function cast(Caster, Target, Amount)
   SetSpellTriggerCount(1, 1)
   AddSpellBonus(Caster, 664, Amount)
   AddProc(Caster, 5, 100)
+  AddProc(Caster, 11, 100)
+  AddProc(Caster, 12, 100)
 end
 
 function proc(Caster, Target, ProcType) 
