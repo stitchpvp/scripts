3b94c44868b834c40fa331ff2e91f3a5a7099744
Rixan <35015924+drakky@users.noreply.github.com>
2018-09-30 23:38:37 -0400

    Update BrucciusCapito.lua

--- SpawnScripts/Freeport/BrucciusCapito.lua
+++ SpawnScripts/Freeport/BrucciusCapito.lua
@@ -12,4 +12,4 @@ function hailed(NPC, Spawn)
   
   AddConversationOption(conversation, "I must meet someone soon and must be going. Good day.")
   StartConversation(conversation, NPC, Spawn, "Helloo there! Can you feel it? The wind is changing direction....")
-endh
+end
