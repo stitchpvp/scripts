5e7283a6b5f69b747c70f3eaab02ebce059781a8
Jonmichael Chambers <jon@tyrbo.net>
2018-10-11 02:57:02 -0600

    Use active toggle with freedom pots

--- Spells/Items/FreedomofAction.lua
+++ Spells/Items/FreedomofAction.lua
@@ -1,6 +1,6 @@
 function cast(Caster, Target)
-  AddImmunitySpell(Caster, 5)
-  AddImmunitySpell(Caster, 6)
+  AddImmunitySpell(Caster, 5, true)
+  AddImmunitySpell(Caster, 6, true)
 end
 
 function remove(Caster, Target)
