24d785b978e26ee40f86d3705d6d77e4b3827c71
Jonmichael Chambers <jon@tyrbo.net>
2018-10-10 20:57:18 -0600

    Update taunt spells to use spell timers rather than spawn timers

--- Spells/Fighter/Crusader/Shadowknight/Blasphemy.lua
+++ Spells/Fighter/Crusader/Shadowknight/Blasphemy.lua
@@ -1,10 +1,10 @@
 function cast(Caster, Target, MinVal, MaxVal, Debuff)
   AddSpellBonus(Target, 202, Debuff)
   Interrupt(Caster, Target)
-  AddControlEffect(Target, 15)
-  AddTimer(Caster, 2000, "remove_taunt_lock")
 
   if IsPlayer(Target) then
+    AddControlEffect(Target, 15)
+    AddSpellTimer(2000, "remove_taunt_lock", Caster, Target)
     SetTarget(Target, Caster)
   else
     AddHate(Caster, Target, math.random(MinVal, MaxVal), 1)
