ab58d5a2b6abd123b74f332dace3808545da906e
Rixan <35015924+drakky@users.noreply.github.com>
2018-10-19 01:30:20 -0400

    Clean up

--- Spells/Items/ResurrectItem.lua
+++ Spells/Items/ResurrectItem.lua
@@ -1,32 +1,17 @@
 function precast(Caster, Target)
-  local check = GetTarget(Caster)
-  local check2 = GetArchetypeName(Target)
-  
   return (not IsInCombat(Caster)), 12
-  
-  if check ~= nil then
-    if check2 == "Priest" then
-	  return true
-    else
-	  SendMessage(Caster, "Target must be priest class.", "yellow")
-	  return false
-    end
-  else
-    return false
-  end
 end
 
 function cast(Caster, Target, Percentage, SpellName)
-  local check = GetArchetypeName(Target)
-  
-  if not IsAlive(Target) then
-    if check == "Priest" then
-      Resurrect(Target, Percentage, Percentage, 1, SpellName, 2, 1)
+  local check = GetTarget(Caster)
+  local check2 = GetArchetypeName(Target)
+
+  if check ~= nil then
+	if not IsAlive(Target) and check2 == "Priest" then
+	  Resurrect(Target, Percentage, Percentage, 1, SpellName, 2, 1)
     else
-      SendMessage(Caster, "Target must be priest class.", "yellow")
-    end
-  else
-    SendMessage(Caster, "Target is not dead.", "yellow")
+	  SendMessage(Caster, "Target must be priest class.", "yellow")
+	end
   end
 end
 
