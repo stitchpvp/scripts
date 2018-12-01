891fc43d0df84cf542711741c585c5c9a9661ed9
Rixan <35015924+drakky@users.noreply.github.com>
2018-12-01 04:53:06 -0500

    Chef Gorga 1.4

--- SpawnScripts/OutpostOfTheOverlord/ChefGorga.lua
+++ SpawnScripts/OutpostOfTheOverlord/ChefGorga.lua
@@ -21,7 +21,7 @@ function hailed(NPC, Spawn)
 	elseif HasQuest(Spawn, 192) and GetQuestStep(Spawn, 192) == 5 then
 		AddConversationOption(conversation, "Back to Tayil!")
 	  StartConversation(conversation, NPC, Spawn, "Heeheehee!  Toss 'em in and smell the Gorga magic.  The stew is done now!  Head back to Tayil, she's waitin' for ya.")
-		SetStepComplete(Spawn, 192, 4)
+		SetStepComplete(Spawn, 192, 5)
 	elseif not HasQuest(Spawn, 192) and HasCompletedQuest(Spawn, 192) then
 		AddConversationOption(conversation, "Uh yeah, livers!")
 		StartConversation(conversation, NPC, Spawn, "Mmm, this stew is yums. Livers!")
