0d32d469e0fcf816900cb495140c8340dcf7668a
Rixan <35015924+drakky@users.noreply.github.com>
2018-12-09 05:01:23 -0500

    BR spell fixes

--- Spells/BattleRoyale/Coalesce.lua
+++ Spells/BattleRoyale/Coalesce.lua
@@ -1,5 +1,6 @@
 function cast(Caster, Target, WardAmt)
   AddWard(Target, WardAmt, 0, 0)
+end
 
 function remove(Caster, Target)
   RemoveWard(Target)
--- Spells/BattleRoyale/HealthPotion.lua
+++ Spells/BattleRoyale/HealthPotion.lua
@@ -1,5 +1,5 @@
 function cast(Caster, Target, Amount)
   SpellHeal(Target, "Heal", Amount, Amount, 2, 1)
-  SendPopUpMessage(Target, "You gained health for picking up the potion.", 242, 152, 142)
-  SendMessage(Target, "\#F2988EYou gained health for picking up the potion.")
+  SendPopUpMessage(Target, "You gained health from picking up the potion.", 242, 152, 142)
+  SendMessage(Target, "\\#F2988EYou gained health from picking up the potion.")
 end
