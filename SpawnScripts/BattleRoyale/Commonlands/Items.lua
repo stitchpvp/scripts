bcb3bfb35a941ea4462828895d2ea03435019873
Rixan <35015924+drakky@users.noreply.github.com>
2018-12-09 17:32:12 -0500

    Attempt to grab spell and item names part 2.

--- SpawnScripts/BattleRoyale/Commonlands/Items.lua
+++ SpawnScripts/BattleRoyale/Commonlands/Items.lua
@@ -17,7 +17,19 @@ function InRange(NPC, Spawn)
   for i, item in ipairs(item_list[math.random(#item_list)]) do
     SummonItem(Spawn, item)
     ApplySpellVisual(Spawn, 591)
-    SendMessage(Spawn, "\\#ffffffYou picked up \\#9989b5[" .. GetName(item) .. "]\\#ffffff.")
+
+    if item == 1 then
+      item_name = "Test"
+    elseif item == 2 then
+      item_name = "Test 2"
+    elseif item == 3 then
+      item_name = "Test 3"
+    elseif item == 4 then
+      item_name = "Test 4"
+    end
+
+    SendMessage(Spawn, "\\#ffffffYou picked up \\#9989b5[" .. item_name .. "]\\#ffffff.")
+
     Despawn(NPC)
   end
 end
--- SpawnScripts/BattleRoyale/Commonlands/Spells.lua
+++ SpawnScripts/BattleRoyale/Commonlands/Spells.lua
@@ -16,7 +16,16 @@ function InRange(NPC, Spawn)
   for i, spell in ipairs(spell_list[math.random(#spell_list)]) do
     AddSpellBookEntry(Spawn, spell, 1)
     ApplySpellVisual(Spawn, 591)
-    SendMessage(Spawn, "\\#ffffffYou picked up \\#9989b5[" .. GetSpellName(spell) .. "]\\#ffffff.")
+
+    if spell == 123626226 then
+      spell_name = "Putrefy"
+    elseif spell == 210633591 then
+      spell_name = "Ancient Shroud"
+    elseif spell == 74638181 then
+      spell_name = "Fuliginous Whip"
+    end
+
+    SendMessage(Spawn, "\\#ffffffYou picked up \\#9989b5[" .. spell_name .. "]\\#ffffff.")
     Despawn(NPC)
   end
 end
