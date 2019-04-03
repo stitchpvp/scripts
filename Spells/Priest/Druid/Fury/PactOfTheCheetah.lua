3e6a63621c420a70479ebcc00b3ba5d62646e8e3
Rixan <35015924+drakky@users.noreply.github.com>
2019-04-03 04:34:09 -0400

    Pact of the Cheetah fix

--- Spells/Priest/Druid/Fury/PactOfTheCheetah.lua
+++ Spells/Priest/Druid/Fury/PactOfTheCheetah.lua
@@ -1,5 +1,5 @@
 function cast(Caster, Target, DmgReduction, Dispel, MovementSpeed)
-  AddSpellBonus(Target, 0, Attribute)
+  --AddSpellBonus(Target, 0, DmgReduction)
   AddSpellBonus(Target, 616, MovementSpeed)
   CureByControlEffect(Target, Dispel, 5, "Cure") -- Root
 end
